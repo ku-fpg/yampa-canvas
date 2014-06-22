95118772388c0170f1abd2176ccc995ffb5a246f
Andy Gill <andygill@ku.edu>
2014-06-22 14:33:22 -0500

    Pushing Neil's code.

---
+++ example/BouncingBalls.hs
@@ -0,0 +1,181 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+-- set browser to: http://localhost:3000/
+import Graphics.Blank hiding (Event)
+import FRP.Yampa
+import FRP.Yampa.Vector2
+import Data.Text(Text)
+
+import FRP.Yampa.Canvas
+
+---------------------------------------------------
+
+main :: IO ()
+main = blankCanvas 3000 animateBalls
+
+-- | Display an animation of multiple falling balls.
+animateBalls :: DeviceContext -> IO ()
+animateBalls = reactimateSFinContext (const ()) renderScene (bouncingBalls someBalls)
+
+---------------------------------------------------
+
+-- We scale such that 0.0 and 1.0 denote the edges of the canvas.
+type Radius = Float
+
+type Acceleration = Vector2 Float
+type Velocity     = Vector2 Float
+type Position     = Vector2 Float
+
+type Colour = Text
+
+---------------------------------------------------
+
+data Ball = MkBall { pos    :: Position,
+                     vel    :: Velocity,
+                     radius :: Radius,
+                     colour :: Colour
+                   }
+
+negateXVel :: Ball -> Ball
+negateXVel b = b { vel = let (x,y) = vector2XY (vel b)
+                          in vector2 (negate x) y
+                 }
+
+negateYVel :: Ball -> Ball
+negateYVel b = b { vel = let (x,y) = vector2XY (vel b)
+                          in vector2 x (negate y)
+                 }
+
+---------------------------------------------------
+
+ball1 :: Ball
+ball1 = MkBall { pos    = vector2 0.1 0.5,
+                 vel    = vector2 0.4 0.1,
+                 radius = 0.05,
+                 colour = "red"
+               }
+
+ball2 :: Ball
+ball2 = MkBall { pos    = vector2 0.3 0.7,
+                 vel    = vector2 (-0.5) (-0.1),
+                 radius = 0.03,
+                 colour = "blue"
+               }
+
+ball3 :: Ball
+ball3 = MkBall { pos    = vector2 0.5 0.4,
+                 vel    = vector2 0.2 1.3,
+                 radius = 0.04,
+                 colour = "green"
+               }
+
+ball4 :: Ball
+ball4 = MkBall { pos    = vector2 0.7 0.8,
+                 vel    = vector2 (-0.1) (-0.2),
+                 radius = 0.06,
+                 colour = "yellow"
+               }
+
+ball5 :: Ball
+ball5 = MkBall { pos    = vector2 0.2 0.9,
+                 vel    = vector2 1.8 0.85,
+                 radius = 0.02,
+                 colour = "orange"
+               }
+
+someBalls :: [Ball]
+someBalls = [ball1,ball2,ball3,ball4,ball5]
+
+gravity :: Acceleration
+gravity = vector2 0 (-1)
+
+---------------------------------------------------
+
+-- | Construct a free-falling ball from an initial ball configuration.
+fallingBall :: Ball -> SF () Ball
+fallingBall b = accelerator gravity (vel b) (pos b) >>> arr updateBall
+  where
+    updateBall :: (Position,Velocity) -> Ball
+    updateBall (p,v) = b { pos = p, vel = v }
+
+-- | Construct a ball that bounces in one dimension from an initial ball configuration.
+bouncingBall1 :: Ball -> SF () Ball
+bouncingBall1 b = switchWhen (fallingBall b) detectFloor f
+  where
+    detectFloor :: SF Ball (Event Ball)
+    detectFloor = edgeWhen p
+      where
+        p :: Ball -> Bool
+        p b1 = (vector2Y (pos b1) <= radius b1) || (vector2Y (pos b1) >= 1 - radius b1)
+
+    f :: Ball -> SF () Ball
+    f b2 = bouncingBall1 (negateYVel b2)
+
+-- | Construct a ball that bounces in two dimensions.
+bouncingBall2 :: Ball -> SF () Ball
+bouncingBall2 b = switchWhen (bouncingBall1 b) detectWall f
+  where
+    detectWall :: SF Ball (Event Ball)
+    detectWall = edgeWhen p
+      where
+        p :: Ball -> Bool
+        p b1 = (vector2X (pos b1) <= radius b1) || (vector2X (pos b1) >= 1 - radius b1)
+
+    f :: Ball -> SF () Ball
+    f b2 = bouncingBall2 (negateXVel b2)
+
+-- | Construct a list of bouncing balls from a list of initial ball configurations.
+bouncingBalls :: [Ball] -> SF () [Ball]
+bouncingBalls bs = parB (map bouncingBall2 bs)
+
+-------------------------------------------------------------------
+
+-- | Draw a circle.
+circle :: Position -> Radius -> Colour -> Canvas ()
+circle pos' r col = do beginPath ()
+                       arc (vector2X pos', vector2Y pos', r, 0, pi*2, False)
+                       closePath ()
+                       fillStyle col
+                       fill ()
+
+-- | A Canvas action to render a single Ball.
+renderBall :: Ball -> Canvas ()
+renderBall b = circle (pos b) (radius b) (colour b)
+
+-- | A Canvas action to render a list of Balls.
+renderBalls :: [Ball] -> Canvas ()
+renderBalls = mapM_ renderBall
+
+-- | A Canvas action to render the entire scene.
+renderScene :: [Ball] -> Canvas ()
+renderScene bs = scaleScene >> renderBalls bs
+
+-- | We scale such that (0,0) is the bottom-left of the canvas and (1,1) is the top-right.
+scaleScene :: Canvas ()
+scaleScene =
+   do context <- myCanvasContext
+      let w = width context
+          h = height context
+      translate (0,h)
+      scale (w, negate h)
+
+-------------------------------------------------------------------
+
+-- Auxillary signal function combinators not provided by the Yampa library.
+
+-- | Given a (constant) 'Acceleration', and an initial 'Velocity' and 'Position',
+-- | produce a signal function that emits the current 'Velocity' and 'Distance'.
+accelerator :: Acceleration -> Velocity -> Position -> SF () (Position,Velocity)
+accelerator a v0 d0 = constant a >>> imIntegral v0 >>> (imIntegral d0 &&& identity)
+
+-- | A variant of 'FRP.Yampa.switch' where the event only depends on the output of the sub-ordinate signal function.
+switchWhen :: SF a b -> SF b (Event e) -> (e -> SF a b) -> SF a b
+switchWhen sf1 sfe = switch (sf1 >>> identity &&& sfe)
+
+-- | A variant of 'FRP.Yampa.edge' which takes a predicate on an input signal, and tags the event with the value of the input signal.
+edgeWhen :: (a -> Bool) -> SF a (Event a)
+edgeWhen p = (p ^>> edge) &&& identity >>^ uncurry tag
+
+-------------------------------------------------------------------
---
+++ src/FRP/Yampa/Canvas.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
+
+module FRP.Yampa.Canvas (reactimateSFinContext) where
+
+import FRP.Yampa
+
+import Data.Time.Clock
+import Data.IORef
+import Control.Concurrent.STM
+
+import Graphics.Blank hiding (Event)
+import qualified Graphics.Blank as Blank
+
+-------------------------------------------------------------------
+
+-- | Redraw the entire canvas.
+renderCanvas ::  DeviceContext -> Canvas () -> IO ()
+renderCanvas context drawAction = send context canvas
+  where
+    canvas :: Canvas ()
+    canvas = do clearCanvas
+                beginPath ()
+                saveRestore drawAction
+
+-------------------------------------------------------------------
+
+type Clock = IORef UTCTime
+
+-- | A specialisation of 'FRP.Yampa.reactimate' to Blank Canvas.
+--   The arguments are: the Canvas action to get input, the Canvas action to emit output, the signal function to be run, and the device context to use.
+reactimateSFinContext :: forall a b. (Blank.Event -> a) -> (b -> Canvas ()) -> SF a b -> DeviceContext -> IO ()
+reactimateSFinContext interpInput putCanvasOutput sf context =
+  do clock <- newClock
+
+     let getInput0 :: IO a
+         getInput0 = return $ interpInput $ Blank.Event { eMetaKey = False, ePageXY = Nothing, eType = "init",  eWhich = Nothing }
+
+         getInput :: Bool -> IO (DTime,Maybe a)
+         getInput canBlock =
+            do let opt_block m = 
+                            if canBlock 
+                            then m
+                            else m `orElse` return Nothing
+               a <- atomically $ opt_block $ do
+                    e <- readTChan (eventQueue context)
+                    return (Just e) 
+               t <- clockTick clock
+               return (t,fmap interpInput a)
+
+         putOutput :: Bool -> b -> IO Bool
+         putOutput changed b = if changed
+                                 then renderCanvas context (putCanvasOutput b) >> return False
+                                 else return False
+
+     reactimate getInput0 getInput putOutput sf
+
+-- | Start a new clock.
+newClock :: IO Clock
+newClock = getCurrentTime >>= newIORef
+
+-- | Compute the time delta since the last clock tick.
+clockTick :: Clock -> IO DTime
+clockTick x =
+    do t0 <- readIORef x
+       t1 <- getCurrentTime
+       writeIORef x t1
+       return (realToFrac (diffUTCTime t1 t0))
+
+-------------------------------------------------------------------
